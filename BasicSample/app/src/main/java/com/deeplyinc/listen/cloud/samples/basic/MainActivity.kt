36e7bb5c15fece1a0005fbee243e31df1a1a5439
Han Park <han@deeplyinc.com>
2023-03-16 03:23:46 +0900

    feat: add sample inference code

--- BasicSample/app/src/main/java/com/deeplyinc/listen/cloud/samples/basic/MainActivity.kt
+++ BasicSample/app/src/main/java/com/deeplyinc/listen/cloud/samples/basic/MainActivity.kt
@@ -5,14 +5,17 @@ import android.content.pm.PackageManager
 import android.media.AudioFormat
 import android.media.AudioRecord
 import android.media.MediaRecorder
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.lifecycleScope
 import com.deeplyinc.listen.cloud.samples.basic.databinding.ActivityMainBinding
+import com.deeplyinc.listen.cloud.sdk.Listen
+import com.deeplyinc.listen.cloud.sdk.ListenEventListener
+import com.deeplyinc.listen.cloud.sdk.models.ClassifierOutput
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
@@ -20,17 +23,24 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var audioRecord: AudioRecord
     private var isRecording = MutableLiveData(false)
-    private val config = Configuration(
-        bufferSize = 48000,
-        sampleRate = 16000,
-        channelMode = AudioFormat.CHANNEL_IN_MONO,
-        encoding = AudioFormat.ENCODING_PCM_16BIT
-    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         initView()
         setContentView(binding.root)
+
+        Listen.getInstance().init("YOUR SDK KEY HERE", "YOUR DEMO NAME HERE")
+        Listen.getInstance().setAsyncInferenceListener(object : ListenEventListener {
+            override fun onDetected(result: ClassifierOutput) {
+                Log.d(TAG, "Asynchronous inference results - listener: $result")
+            }
+        })
+        lifecycleScope.launch(Dispatchers.Default) {
+            Listen.getInstance().getResultFlow().collect() {
+                Log.d(TAG, "Asynchronous inference results - flow: $it")
+            }
+        }
+
         requestRecordingPermission()
     }
 
@@ -64,31 +74,34 @@ class MainActivity : AppCompatActivity() {
         ) {
             return
         }
+        val bufferSize = Listen.getInstance().getInferenceParams().minInputSize
         audioRecord = AudioRecord(
             MediaRecorder.AudioSource.MIC,
-            config.sampleRate,
-            config.channelMode,
-            config.encoding,
-            config.bufferSize
+            Listen.getInstance().getInferenceParams().sampleRate,
+            AudioFormat.CHANNEL_IN_MONO,
+            AudioFormat.ENCODING_PCM_16BIT,
+            bufferSize,
         )
         if (audioRecord.state == AudioRecord.STATE_UNINITIALIZED) {
             Log.w(TAG, "Failed to initialize AudioRecord")
             return
         }
 
-        val buffer = ShortArray(config.bufferSize)
+        val buffer = ShortArray(bufferSize)
+
         audioRecord.startRecording()
-        lifecycleScope.launch(Dispatchers.Default) {
+        lifecycleScope.launch(Dispatchers.IO) {
             while (isRecording.value == true) {
                 audioRecord.read(buffer, 0, buffer.size)
-                doAnythingWith(buffer)
+
+                val result = Listen.getInstance().inference(buffer)
+                Log.d(TAG, "Synchronous inference result: $result")
+
+                Listen.getInstance().inferenceAsync(buffer)
             }
+
             audioRecord.stop()
         }
-    }
-
-    private fun doAnythingWith(buffer: ShortArray) {
-        /* Use recorded data here */
     }
 
     companion object {
